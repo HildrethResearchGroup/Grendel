48f6b09f3ba536705c9c6c2babefd6a1853357f7
Allison Smith <allys7@protonmail.com>
2021-05-26 12:33:32 -0600

    Refactored toolbar to move majority of code outside of the main ContentView

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,6 +7,19 @@
 
 import SwiftUI
 
+struct ContentView: View {
+    @Binding var document: OutlinerDocument
+    
+    var body: some View {
+        Text("Tree goes here")
+            .font(.title)
+            .frame(minWidth: 400, idealWidth: 600, maxWidth: .infinity, minHeight: 300, idealHeight: 400, maxHeight: .infinity, alignment: .center)
+            .toolbar(content: {
+                Toolbar()
+            })
+            .focusable()
+    }
+}
 
 //Toolbar functions
 func deleteAction() {
@@ -49,55 +62,33 @@ func fontAction() {
     print("Change font")
 }
 
-
-
-
-struct ContentView: View {
-    @Binding var document: OutlinerDocument
-    
+struct Toolbar: View {
     var body: some View {
-        Text("Tree goes here")
-            .font(.title)
-            .frame(minWidth: 400, idealWidth: 600, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 300, idealHeight: 400, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
-            .toolbar(content: {
-                // main buttons to modify the model
-                //FIXME: Enter doesn't work in full screen
-                
-                    HStack(alignment: .bottom) {
-                        ActionButton(imageName: "text.badge.minus", label: "Delete item(s)", customAction: deleteAction)
-                            .keyboardShortcut(.delete, modifiers: [.shift])
-                        ActionButton(imageName: "text.badge.plus", label: "Add item", customAction: addItemAction)
-                            .keyboardShortcut(.return, modifiers: [])
-                        ActionButton(imageName: "text.badge.star", label: "Add child", customAction: addChildAction)
-                            .keyboardShortcut(.return, modifiers: [.shift])
-                        ActionButton(imageName: "arrow.right.to.line", label: "Indent", customAction: indentAction)
-                            .keyboardShortcut(.tab, modifiers: [])
-                        ActionButton(imageName: "arrow.left.to.line", label: "Outdent", customAction: outdentAction)
-                            .keyboardShortcut(.tab, modifiers: [.shift])
-                    }
-                Spacer()
-                
-                HStack(alignment: .bottom){
-                    
-                    ActionButton(imageName: "pencil", label: "Edit Note", customAction: editAction)
-                    ActionButton(imageName: "eye", label: "Toggle Children", customAction: toggleAction)
-                    ActionButton(imageName: "eyedropper", label: "Label", customAction: labelAction)
-
-                    
-                }
-                
-                Spacer()
-                
-                HStack(alignment: .bottom){
-                    
-                    ActionButton(imageName: "paintbrush.fill", label: "Colors", customAction: colorAction)
-                    ActionButton(imageName: "character", label: "Font", customAction: fontAction)
-
-                    
-                }
-                
-            })
-            .focusable()
+        // main buttons to modify the model
+        //FIXME: Enter doesn't work in full screen
+        HStack(alignment: .center) {
+            ActionButton(imageName: "text.badge.minus", label: "Delete item(s)", customAction: deleteAction)
+                .keyboardShortcut(.delete, modifiers: [.shift])
+            ActionButton(imageName: "text.badge.plus", label: "Add item", customAction: addItemAction)
+                .keyboardShortcut(.return, modifiers: [])
+            ActionButton(imageName: "text.badge.star", label: "Add child", customAction: addChildAction)
+                .keyboardShortcut(.return, modifiers: [.shift])
+            ActionButton(imageName: "arrow.right.to.line", label: "Indent", customAction: indentAction)
+                .keyboardShortcut(.tab, modifiers: [])
+            ActionButton(imageName: "arrow.left.to.line", label: "Outdent", customAction: outdentAction)
+                .keyboardShortcut(.tab, modifiers: [.shift])
+        }
+        Spacer()
+        HStack(alignment: .bottom){
+            ActionButton(imageName: "pencil", label: "Edit Note", customAction: editAction)
+            ActionButton(imageName: "eye", label: "Toggle Children", customAction: toggleAction)
+            ActionButton(imageName: "eyedropper", label: "Label", customAction: labelAction)
+        }
+        Spacer()
+        HStack(alignment: .bottom){
+            ActionButton(imageName: "paintbrush.fill", label: "Colors", customAction: colorAction)
+            ActionButton(imageName: "character", label: "Font", customAction: fontAction)
+        }
     }
 }
 
@@ -107,7 +98,7 @@ struct ActionButton: View {
     var imageName: String
     var label: String
     var customAction: () -> Void
-
+    
     var body: some View {
         Button(action: customAction, label: {
             Image(systemName: imageName)
